8eea223aabdac2b99bf262f1ce53fbfefe68d7ea
leabol <1791722441@qq.com>
2025-11-16 02:27:59 +0800

    新增Channel类，实现事件处理和通道管理功能

---
+++ src/Channel.cpp
@@ -0,0 +1,48 @@
+#include "../include/Channel.hpp"
+
+#include <sys/epoll.h>
+
+#include "../include/EventLoop.hpp"
+#include "../include/Log.hpp"
+
+using namespace Server;
+
+void Channel::update() {
+    loop_->updateChannel(this);
+}
+
+void Channel::handleEvent() {
+    const uint32_t rev = revents_;
+
+    // 错误或挂断优先
+    if (rev & (EPOLLERR | EPOLLHUP)) {
+        LOG_WARN("fd:{}, channel handleEvent() EPOLLUP/EPOLLERR", fd_);
+        if (closeCallback_) {
+            closeCallback_();  // 多数场景下读到 0 即可判定关闭
+        }
+        return;
+    }
+    // 半关闭
+    if (rev & EPOLLRDHUP) {
+        LOG_WARN("fd:{}, channel handleEvent() EPOLLRDHUP", fd_);
+        if (readCallback_) {
+            readCallback_();  // 读到 0，按关闭处理
+        }
+    }
+    // 可读
+    if (rev & EPOLLIN) {
+        if (readCallback_) {
+            readCallback_();
+        }
+    }
+    // 可写
+    if (rev & EPOLLOUT) {
+        if (writeCallback_) {
+            writeCallback_();
+        }
+    }
+}
+
+void Channel::remove() {
+    loop_->removeChannel(this);
+}
